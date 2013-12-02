56ffc2beaf6a0c8a8f697e32414b0001dfbfb7cd
sbashtavenko <sbashtavenko@live.com>
2013-12-02 16:46:25 -0500

    QueueWithTwoStacks

---
+++ Code/LinkedListsStacksAndQueues/QueueWithTwoStacks.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code.LinkedListsStacksAndQueues
+{
+  public class QueueWithTwoStacks<T>
+  {
+    private Stack<T> _inbound;
+    private Stack<T> _outbound;
+
+    public QueueWithTwoStacks()
+    {
+      _inbound = new Stack<T>();
+      _outbound = new Stack<T>();
+    }
+
+    public void Enqueue(T item)
+    {
+      _inbound.Push(item);
+    }
+
+    public T Dequeue()
+    {
+      if (_outbound.Count == 0)
+      {
+        while (_inbound.Count > 0)
+        {
+          _outbound.Push(_inbound.Pop());
+        }
+      }
+      return _outbound.Pop();
+    }
+  }
+}
