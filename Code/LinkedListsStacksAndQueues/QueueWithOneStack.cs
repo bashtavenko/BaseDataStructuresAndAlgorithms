7f3385071dea53286765ac54dbba0601d6e54349
sbashtavenko <sbashtavenko@live.com>
2013-12-20 16:11:09 -0500

    QueueWithOneStack.cs

---
+++ Code/LinkedListsStacksAndQueues/QueueWithOneStack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code.LinkedListsStacksAndQueues
+{
+  public class QueueWithOneStack<T>
+  {
+    private readonly Stack<T> _stack;
+
+    public QueueWithOneStack()
+    {
+      _stack = new Stack<T>();
+    }
+
+    public void Enqueue(T item)
+    {
+      _stack.Push(item);
+    }
+
+    public T Dequeue()
+    {
+      T x, res;
+
+      if (_stack.Count == 1)
+        return _stack.Pop();
+      else
+      {
+        x = _stack.Pop();
+        res = Dequeue();
+        _stack.Push(x);
+        return res;
+      }
+    }    
+  }
+}
---
+++ UnitTests/QueueWithStackTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Code.LinkedListsStacksAndQueues;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+  [TestClass]
+  public class QueueWithStackTest
+  {
+    [TestMethod]
+    public void QueueWithStack()
+    {
+      var q = new QueueWithOneStack<int>();
+      q.Enqueue(10);
+      q.Enqueue(20);
+      q.Enqueue(30);
+
+      Assert.AreEqual(10, q.Dequeue());
+      Assert.AreEqual(20, q.Dequeue());
+      Assert.AreEqual(30, q.Dequeue());
+    }
+  }
+}
